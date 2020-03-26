d608c5ffa58f6afe19ef5f53b459b4f93f56f3fd
echasin <echasin@innvosolutions.com>
2020-03-25 19:52:48 -0700

    parseSegments works for a simple Array

--- experiment-3/scripts/parseSegments.py
+++ experiment-3/scripts/parseSegments.py
@@ -1,22 +1,25 @@
 import json
-
+from pprint import pprint 
 from jsonpath_ng import jsonpath, parse
 
-#json_string = '{"palette":[{"color": "red", "value": "#f00"}]}'
-json_string = '[{"color": "red", "value": "#f00"}]'
-json_data = json.loads(json_string)
-json_array = json.loads(json_string)
+#Read in data file
+with open('../input/colorArray.json', 'r') as data_file:
+    json_data = json.loads(data_file.read())
+#Create Array for items in json_date
 items_list = []
 
-for item in json_array:
-    counter = 0
+#Set Counter - for loop should be updated
+counter = -1
+itemcount = len(json_data)
+
+for item in json_data:
+    counter = counter + 1
     item_details = {"color":None, "value":None}#intializing item_details
-    print (counter)
     item_details['counter'] = counter
     item_details['color'] = item['color']
     item_details['value'] = item['value']
     items_list.append(item_details)
-print (items_list)
+pprint (items_list)
 #jsonpath_expression = parse('$.id')
 
 #match = jsonpath_expression.find(json_data)
