a0632cee14d7590411d481db0557eb6dfb38d859
Eric Chasin <echasin@innvosolutions.com>
2020-04-10 15:17:00 -0700

    WIP - moved mediaConstraints to main.js

--- experiment-4/aws-demo/public/javascripts/main.js
+++ experiment-4/aws-demo/public/javascripts/main.js
@@ -6,16 +6,3 @@ var mediaConstraints = {
 };
 console.log('mediaConstraints', mediaConstraints)
 
-
-//mediaContstaints {audio.true},  onMediaSucess(function),  onMediaError(function)
-navigator.getUserMedia(mediaConstraints, onMediaSuccess, onMediaError);
-
-function onMediaSuccess(stream) {
-    var mediaRecorder = new MediaStreamRecorder(stream);// Requires js library cdn.webrtc-experiment.com/MediaStreamRecorder.js
-    mediaRecorder.mimeType = 'audio/webm'; // audio/webm or audio/ogg or audio/wav
-
-    console.log('function OnMediaSuccessmediaRecorder.mimeType = ', mediaRecorder.mimeType  )
-}
-
-function onMediaError(stream) {
-}
