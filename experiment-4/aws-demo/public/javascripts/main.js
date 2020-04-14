6355288bd4bd75111af5fccf2bae5c0cb2c5593d
Chasin <eric.r.chasin@uscis.dhs.gov>
2020-04-14 10:01:06 -0700

    Updated - Input Device selectable

--- experiment-4/aws-demo/public/javascripts/main.js
+++ experiment-4/aws-demo/public/javascripts/main.js
@@ -1,18 +1,12 @@
-console.log('In: main.js')
+'use strict';
 
-//Initialize variable
-//UI Selectors
+// const videoElement = document.querySelector('video');
 const audioInputSelect = document.querySelector('select#audioSource');
 const selectors = [audioInputSelect];
-
 var mediaConstraints = {
   audio: true
 };
-//Log out variables
-console.log('mediaConstraints', mediaConstraints)
-
-
-
+//NEW CODE
 function captureUserMedia(mediaConstraints, successCallback, errorCallback) {
   navigator.mediaDevices.getUserMedia(mediaConstraints).then(successCallback).catch(errorCallback);
 }
@@ -22,6 +16,7 @@ var mediaRecorder;
 
 function onMediaSuccess(stream) {
   var audio = document.createElement('audio');
+  
 
   audio = mergeProps(audio, {
     controls: true,
@@ -91,20 +86,99 @@ window.onbeforeunload = function () {
   document.querySelector('#start-recording').disabled = false;
 };
 
-function start() {
-  console.log("In function start()");
+// audioOutputSelect.disabled = !('sinkId' in HTMLMediaElement.prototype);
 
+function gotDevices(deviceInfos) {
+  // Handles being called several times to update labels. Preserve values.
+  const values = selectors.map(select => select.value);
+  selectors.forEach(select => {
+    while (select.firstChild) {
+      select.removeChild(select.firstChild);
+    }
+  });
+  for (let i = 0; i !== deviceInfos.length; ++i) {
+    const deviceInfo = deviceInfos[i];
+    const option = document.createElement('option');
+    option.value = deviceInfo.deviceId;
+    if (deviceInfo.kind === 'audioinput') {
+      option.text = deviceInfo.label || `microphone ${audioInputSelect.length + 1}`;
+      audioInputSelect.appendChild(option);
+    } else {
+      // console.log('Some other kind of source/device: ', deviceInfo);
+    }
+  }
+  selectors.forEach((select, selectorIndex) => {
+    if (Array.prototype.slice.call(select.childNodes).some(n => n.value === values[selectorIndex])) {
+      select.value = values[selectorIndex];
+    }
+  });
+}
+
+navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
+
+// Attach audio output device to video element using device/sink ID.
+function attachSinkId(element, sinkId) {
+  if (typeof element.sinkId !== 'undefined') {
+    element.setSinkId(sinkId)
+        .then(() => {
+          console.log(`Success, audio output device attached: ${sinkId}`);
+        })
+        .catch(error => {
+          let errorMessage = error;
+          if (error.name === 'SecurityError') {
+            errorMessage = `You need to use HTTPS for selecting audio output device: ${error}`;
+          }
+          console.error(errorMessage);
+          // Jump back to first output device in the list as it's the default.
+          audioOutputSelect.selectedIndex = 0;
+        });
+  } else {
+    console.warn('Browser does not support output device selection.');
+  }
+}
+
+function changeAudioDestination() {
+  const audioDestination = audioOutputSelect.value;
+  attachSinkId(videoElement, audioDestination);
+}
+
+function gotStream(stream) {
+  window.stream = stream; // make stream available to console
+  videoElement.srcObject = stream;
+  // Refresh button list in case labels have become available
+  return navigator.mediaDevices.enumerateDevices();
+}
+
+function handleError(error) {
+  console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
+}
+
+function start() {
+ 
   navigator.mediaDevices.enumerateDevices()
     .then(function (devices) {
-      devices.forEach(function (device) {
-        // console.log(device.kind + ": " + device.label +
-        //   " id = " + device.deviceId);
+      devices.forEach(function (device) {;
         console.log(device.kind + ": " + device.label);
       });
     })
   function getConnnectedDevices() {
     console.log("function getConnnectedDevices()")
   }
+
+  if (window.stream) {
+    window.stream.getTracks().forEach(track => {
+      track.stop();
+    });
+  }
+  const audioSource = audioInputSelect.value;
+
+  const constraints = {
+    audio: {deviceId: audioSource ? {exact: audioSource} : undefined}
+  };
+  console.log('Device Selected :', audioSource );
+  navigator.mediaDevices.getUserMedia(constraints).then(gotStream).then(gotDevices).catch(handleError);
 }
+
+audioInputSelect.onchange = start;
 
 start();
