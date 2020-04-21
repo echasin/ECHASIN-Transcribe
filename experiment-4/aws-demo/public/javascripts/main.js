c9c232c0ce91d54017a30de2650f67b86ee1f4ef
joelcasillas <Raiders182>
2020-04-20 22:53:22 -0700

    Audio Input Select Progress Made

--- experiment-4/aws-demo/public/javascripts/main.js
+++ experiment-4/aws-demo/public/javascripts/main.js
@@ -1,92 +1,75 @@
+/*
+*  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
+*
+*  Use of this source code is governed by a BSD-style license
+*  that can be found in the LICENSE file in the root of the source
+*  tree.
+*/
+
 'use strict';
 
-// const videoElement = document.querySelector('video');
+
 const audioInputSelect = document.querySelector('select#audioSource');
 const selectors = [audioInputSelect];
 var mediaConstraints = {
-  audio: true
-};
-//NEW CODE
+    audio: true
+  };
+
 function captureUserMedia(mediaConstraints, successCallback, errorCallback) {
   navigator.mediaDevices.getUserMedia(mediaConstraints).then(successCallback).catch(errorCallback);
 }
 
-//onMediaSuccess
+  //onMediaSuccess
 var mediaRecorder;
-
 function onMediaSuccess(stream) {
   var audio = document.createElement('audio');
-  
-
   audio = mergeProps(audio, {
     controls: true,
     muted: true
   });
   audio.srcObject = stream;
   audio.play();
-
   audiosContainer.appendChild(audio);
   audiosContainer.appendChild(document.createElement('hr'));
-
   mediaRecorder = new MediaStreamRecorder(stream);
   mediaRecorder.stream = stream;
-
   var recorderType = document.getElementById('audio-recorderType').value;
-
   if (recorderType === 'MediaRecorder API') {
     mediaRecorder.recorderType = MediaRecorderWrapper;
   }
-
   if (recorderType === 'WebAudio API (WAV)') {
     mediaRecorder.recorderType = StereoAudioRecorder;
     mediaRecorder.mimeType = 'audio/wav';
   }
-
   if (recorderType === 'WebAudio API (PCM)') {
     mediaRecorder.recorderType = StereoAudioRecorder;
     mediaRecorder.mimeType = 'audio/pcm';
   }
-
   // don't force any mimeType; use above "recorderType" instead.
   // mediaRecorder.mimeType = 'audio/webm'; // audio/ogg or audio/wav or audio/webm
-
   mediaRecorder.audioChannels = !!document.getElementById('left-channel').checked ? 1 : 2;
   mediaRecorder.ondataavailable = function (blob) {
-
     console.log('Create audiosContainer');//Create audiosContainer
-
     var a = document.createElement('a');
     a.target = '_blank';
-    a.innerHTML = 'Open Recorded Audio No. ' + (index++) + ' (Size: ' + bytesToSize(blob.size) + ') Time Length: ' + getTimeLength(timeInterval);
-
+    a.innerHTML = 'Mic #1 Open Recorded Audio No. ' + (index++) + ' (Size: ' + bytesToSize(blob.size) + ') Time Length: ' + getTimeLength(timeInterval);
     a.href = URL.createObjectURL(blob);
-
     audiosContainer.appendChild(a);
     audiosContainer.appendChild(document.createElement('hr'));
   };
-
   var timeInterval = document.querySelector('#time-interval').value;
   if (timeInterval) timeInterval = parseInt(timeInterval);
   else timeInterval = 5 * 1000;
-
   // get blob after specific time interval
   mediaRecorder.start(timeInterval);
-
   document.querySelector('#stop-recording').disabled = false;
   document.querySelector('#pause-recording').disabled = false;
   document.querySelector('#save-recording').disabled = false;
 }
 
-//function onMediaError
 function onMediaError(e) {
   console.error('media error', e);
 }
-
-window.onbeforeunload = function () {
-  document.querySelector('#start-recording').disabled = false;
-};
-
-// audioOutputSelect.disabled = !('sinkId' in HTMLMediaElement.prototype);
 
 function gotDevices(deviceInfos) {
   // Handles being called several times to update labels. Preserve values.
@@ -104,7 +87,7 @@ function gotDevices(deviceInfos) {
       option.text = deviceInfo.label || `microphone ${audioInputSelect.length + 1}`;
       audioInputSelect.appendChild(option);
     } else {
-      // console.log('Some other kind of source/device: ', deviceInfo);
+      console.log(deviceInfo);
     }
   }
   selectors.forEach((select, selectorIndex) => {
@@ -137,10 +120,6 @@ function attachSinkId(element, sinkId) {
   }
 }
 
-function changeAudioDestination() {
-  const audioDestination = audioOutputSelect.value;
-  attachSinkId(videoElement, audioDestination);
-}
 
 function gotStream(stream) {
   window.stream = stream; // make stream available to console
@@ -154,28 +133,14 @@ function handleError(error) {
 }
 
 function start() {
- 
-  navigator.mediaDevices.enumerateDevices()
-    .then(function (devices) {
-      devices.forEach(function (device) {;
-        console.log(device.kind + ": " + device.label);
-      });
-    })
-  function getConnnectedDevices() {
-    console.log("function getConnnectedDevices()")
-  }
-
   if (window.stream) {
     window.stream.getTracks().forEach(track => {
       track.stop();
     });
   }
   const audioSource = audioInputSelect.value;
-
   const constraints = {
-    audio: {deviceId: audioSource ? {exact: audioSource} : undefined}
-  };
-  console.log('Device Selected :', audioSource );
+    audio: {deviceId: audioSource ? {exact: audioSource} : undefined}  };
   navigator.mediaDevices.getUserMedia(constraints).then(gotStream).then(gotDevices).catch(handleError);
 }
 
